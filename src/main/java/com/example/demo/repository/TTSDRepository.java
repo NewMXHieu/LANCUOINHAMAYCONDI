2f9773b0aa3be8660ee7cf0f54a104855d1a5f1a
truongtienanh12 <tienanh19122002@gmail.com>
2024-05-20 03:37:26 +0700

    fix merge

--- src/main/java/com/example/demo/repository/TTSDRepository.java
+++ src/main/java/com/example/demo/repository/TTSDRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.ThanhVien;
 import com.example.demo.entity.ThietBi;
 import com.example.demo.entity.ThongTinSD;
 import org.springframework.data.domain.Page;
@@ -7,7 +8,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface TTSDRepository extends JpaRepository<ThongTinSD, Integer> {
     @Query("select distinct tb from ThongTinSD ttsd right outer join ttsd.thietBi tb where ttsd.tgDatCho is null")
     Page<ThietBi> getAll(Pageable pageable);
+
+    List<ThongTinSD> findByThanhVienMaTV(int maTV);
+
 }
